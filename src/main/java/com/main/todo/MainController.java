5b473fa58cf50a76e87c9d723599592209ecf7a4
KimYoonMin <dbsalszz@naver.com>
2025-09-21 21:29:42 +0900

    example of thymeleaf

--- src/main/java/com/main/todo/MainController.java
+++ src/main/java/com/main/todo/MainController.java
@@ -1,12 +1,12 @@
 package com.main.todo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 @RequestMapping("/api/main")
 public class MainController {
 
@@ -14,10 +14,8 @@ public class MainController {
     private MainService mainService;
 
     @GetMapping("/test")
-    public String test() {
-        Member member = new Member();
-        member.setId(Long.parseLong("1022"));
-        member.getAge();
+    public String test(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
         return "test";
     }
 }
