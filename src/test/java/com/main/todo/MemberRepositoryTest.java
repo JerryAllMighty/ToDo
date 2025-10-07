66e1b5b3cc0c9390384987517fdba7a9f33331a8
KimYoonMin <dbsalszz@naver.com>
2025-10-07 21:32:25 +0900

    test  기초

--- src/test/java/com/main/todo/MemberRepositoryTest.java
+++ src/test/java/com/main/todo/MemberRepositoryTest.java
@@ -15,10 +15,10 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
-    public void test() {
+    public void saveTest() {
         //given
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
 
         //when
         Long savedId = memberRepository.save(member);
@@ -26,10 +26,30 @@ class MemberRepositoryTest {
 
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
         Assertions.assertThat(findMember).isEqualTo(member);
         //같은 영속성 컨텍스트 안에서는 id 값이 같으면 같은 객체라고 간주
         System.out.println("findMember == member" + (findMember == member));
+
+    }
+
+    @Test
+    @Transactional
+    public void saveTestForTDD() {
+        //테스트 작성
+        // 실행 가능하게
+        //올바르게
+        //먼저 작동하게, 그 다음 깔끔하게
+
+        //설계상의 결함을 그 결함으로 인해 실패하는 테스트로 변환
+        //스텁 구현
+        //올바른 코드 테스트
+
+        //given
+
+        //when
+
+        //then
 
     }
 
