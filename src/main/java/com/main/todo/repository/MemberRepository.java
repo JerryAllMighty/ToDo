1612d612a4d8016a6422939e320001b895f37c3f
KimYoonMin <dbsalszz@naver.com>
2025-10-16 16:14:32 +0900

    회원 레포지토리 수정

--- src/main/java/com/main/todo/repository/MemberRepository.java
+++ src/main/java/com/main/todo/repository/MemberRepository.java
@@ -1,22 +1,45 @@
-package com.main.todo;
+package com.main.todo.repository;
 
 import com.main.todo.domain.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class MemberRepository {
 
-    @PersistenceContext
+    @PersistenceContext //해당 Bean이 있다면 스프링이 생성한 엔티티 매니저를 주입해준다
     private EntityManager em;
+
+
+//    @PersistenceUnit //해당 Bean이 있다면 스프링이 생성한 엔티티 매니저 팩토리를 주입해준다
+//    private EntityManagerFactory emf;
+
 
     public Long save(Member member) {
         em.persist(member);
         return member.getId();
     }
 
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        //SQL 이랑 다른 건, jpql은 객체에 대한 조회
+        List<Member> result = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        return result;
+    }
+
+    public List<Member> findByName(String name){
+        List<Member> result = em.createQuery("select  m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+
+        return result;
+
     }
 }
