9d7b762ea0de710aaa4ce58c503c729b06bc17e3
KimYoonMin <dbsalszz@naver.com>
2025-10-30 17:34:47 +0900

    상품 Repository 추가

---
+++ src/main/java/com/main/todo/repository/ItemRepository.java
@@ -0,0 +1,36 @@
+package com.main.todo.repository;
+
+import com.main.todo.domain.Member;
+import com.main.todo.domain.item.Item;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        String jpql = "select i from Item i";
+        return em.createQuery(jpql, Item.class)
+                .getResultList();
+    }
+
+
+}
