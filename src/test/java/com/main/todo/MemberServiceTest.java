0c9551bae8aaf7ad15f8aeefb1277b37c28be921
KimYoonMin <dbsalszz@naver.com>
2025-10-30 15:03:14 +0900

    회원 서비스 계층 테스트 추가

---
+++ src/test/java/com/main/todo/MemberServiceTest.java
@@ -0,0 +1,29 @@
+package com.main.todo;
+
+import com.main.todo.repository.MemberRepository;
+import com.main.todo.service.MemberService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+    }
+
+    @Test
+    void 중복_회원_예외() {
+    }
+}
