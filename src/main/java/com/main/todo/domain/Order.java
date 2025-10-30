26bd4088ef1898d9956388ebbb1c2d8addae1ffb
KimYoonMin <dbsalszz@naver.com>
2025-10-30 18:22:30 +0900

    Order 엔티티 로직 추가

--- src/main/java/com/main/todo/domain/Order.java
+++ src/main/java/com/main/todo/domain/Order.java
@@ -3,7 +3,6 @@ package com.main.todo.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,7 +34,7 @@ public class Order {
 
     private LocalDateTime orderDate;
 
-    private String status;
+    private OrderStatus status;
 
     //연관관계 편의 메서드
     public void setMember(Member member) {
@@ -52,5 +51,47 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
 
+    }
+
+    /**
+     * 생성 메서드
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    /**
+     * 주문 취소
+     */
+    public void cancelOrder() {
+        if (delivery.getStatus().equals(DeliveryStatus.COMP)) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소할 수 없습니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItemList) {
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItemList) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
