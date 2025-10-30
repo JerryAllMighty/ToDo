c2223d994b3d74a721a1a410a5190aa306fba7fa
KimYoonMin <dbsalszz@naver.com>
2025-10-30 15:51:00 +0900

    중복 회원 예외 테스트 작성

--- src/test/java/com/main/todo/MemberServiceTest.java
+++ src/test/java/com/main/todo/MemberServiceTest.java
@@ -4,7 +4,6 @@ import com.main.todo.domain.Member;
 import com.main.todo.repository.MemberRepository;
 import com.main.todo.service.MemberService;
 import jakarta.transaction.Transactional;
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,7 +22,7 @@ class MemberServiceTest {
 
     @Test
     void 회원가입() {
-        Member member =  new Member();
+        Member member = new Member();
         member.setName("Kim");
 
         long savedId = memberRepository.save(member);
@@ -33,6 +32,20 @@ class MemberServiceTest {
     }
 
     @Test
-    void 중복_회원_예외() {
+    void 중복_회원_예외() throws Exception {
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e) {
+            return;
+        }
+//        assertThrows(new IllegalStateException, memberService.join(member2));
+        fail("예외가 발생해야한다");
     }
 }
