5e0a98d3022eb0f595cd70be6c0d785aebcdb63f
KimYoonMin <dbsalszz@naver.com>
2025-10-31 18:06:59 +0900

    회원가입 뷰템플릿용 컨트롤러 추가

---
+++ src/main/java/com/main/todo/controller/MemberController.java
@@ -0,0 +1,22 @@
+package com.main.todo.controller;
+
+import com.main.todo.domain.MemberForm;
+import com.main.todo.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+}
