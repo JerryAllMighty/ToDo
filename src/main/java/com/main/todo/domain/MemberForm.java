f1624549291bce36b0e7d363142a0f6048ec3e45
KimYoonMin <dbsalszz@naver.com>
2025-10-31 18:06:47 +0900

    회원가입 객체 추가

---
+++ src/main/java/com/main/todo/domain/MemberForm.java
@@ -0,0 +1,15 @@
+package com.main.todo.domain;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수입니다")
+    private String name;
+
+    private Address address;
+}
