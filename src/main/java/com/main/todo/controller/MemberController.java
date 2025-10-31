1951523723df5b7634dac37ff20226da0014d9aa
KimYoonMin <dbsalszz@naver.com>
2025-10-31 20:25:26 +0900

    회원가입 컨트롤러 메서드 추가

--- src/main/java/com/main/todo/controller/MemberController.java
+++ src/main/java/com/main/todo/controller/MemberController.java
@@ -1,12 +1,18 @@
 package com.main.todo.controller;
 
+import com.main.todo.domain.Address;
+import com.main.todo.domain.Member;
 import com.main.todo.domain.MemberForm;
 import com.main.todo.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
@@ -18,5 +24,20 @@ public class MemberController {
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "members/createMemberForm";
+
+        }
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipCode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
