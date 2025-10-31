31b4edb9823e96b07ea1252dc9acddac1a4b912b
KimYoonMin <dbsalszz@naver.com>
2025-10-31 21:22:32 +0900

    상품 뷰템플릿 컨트롤러 추가

---
+++ src/main/java/com/main/todo/controller/ItemController.java
@@ -0,0 +1,48 @@
+package com.main.todo.controller;
+
+import com.main.todo.domain.BookForm;
+import com.main.todo.domain.item.Item;
+import com.main.todo.service.ItemService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.main.todo.domain.item.Book;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(@Valid BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getIsbn());
+
+        itemService.save(book);
+
+        return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> itemList = itemService.findItems();
+        model.addAttribute("items", itemList);
+        return "items/itemList";
+    }
+
+}
