43de6e086abf49a1a3216483237a96b6567d109d
KimYoonMin <dbsalszz@naver.com>
2025-11-01 17:31:45 +0900

    주문 Controller 주문 목록 조회 API 추가

--- src/main/java/com/main/todo/controller/OrderController.java
+++ src/main/java/com/main/todo/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.main.todo.controller;
 
 import com.main.todo.domain.Member;
+import com.main.todo.domain.Order;
+import com.main.todo.domain.OrderSearch;
 import com.main.todo.domain.item.Item;
 import com.main.todo.service.ItemService;
 import com.main.todo.service.MemberService;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -43,5 +46,13 @@ public class OrderController {
         orderService.order(memberId, itemId, count);
 
         return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String list(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders= orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
     }
 }
