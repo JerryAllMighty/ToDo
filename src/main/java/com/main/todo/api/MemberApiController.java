70efef92122e3b062d713ad500fc819a14068080
KimYoonMin <dbsalszz@naver.com>
2025-11-06 16:39:10 +0900

    REST API 용 memberApiController 추가

---
+++ src/main/java/com/main/todo/api/MemberApiController.java
@@ -0,0 +1,67 @@
+package com.main.todo.api;
+
+import com.main.todo.domain.Member;
+import com.main.todo.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+//@Controller
+//@ResponseBody
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest createMemberRequest) {
+        Member member = new Member();
+        member.setName(createMemberRequest.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse saveMemberV2(@PathVariable("id") Long id,
+                                             @RequestBody @Valid UpdateMemberRequest updateMemberRequest) {
+        memberService.update(id, updateMemberRequest.getName());
+        Member foundMember = memberService.findOne(id);
+        return new UpdateMemberResponse(id, foundMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
