1a4272b28343b92b2fee01e61f21e8eeb87f0240
KimYoonMin <dbsalszz@naver.com>
2025-11-06 18:27:50 +0900

    Member 객체 조회 메서드 추가 (엔티티 활용 리턴 예제, DTO 활용 리턴 예제)

--- src/main/java/com/main/todo/api/MemberApiController.java
+++ src/main/java/com/main/todo/api/MemberApiController.java
@@ -9,12 +9,43 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 //@Controller
 //@ResponseBody
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> members = memberService.findMembers();
+        List<MemberDto> collect = members.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect, collect.size());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+        private int size;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
