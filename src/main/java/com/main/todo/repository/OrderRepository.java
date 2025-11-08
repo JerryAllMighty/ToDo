d414a1a6dac79b82501dbc8b04d6513eb8ad9bad
KimYoonMin <dbsalszz@naver.com>
2025-11-08 18:54:46 +0900

    주문 목록 조회 시 fetch join 활용 메서드 추가

--- src/main/java/com/main/todo/repository/OrderRepository.java
+++ src/main/java/com/main/todo/repository/OrderRepository.java
@@ -28,4 +28,11 @@ public class OrderRepository {
         return em.createQuery("select o from Order o join o.member m", Order.class)
                 .getResultList();
     }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery("select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .getResultList();
+    }
 }
