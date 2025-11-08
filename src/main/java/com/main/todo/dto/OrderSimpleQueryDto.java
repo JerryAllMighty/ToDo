516124372207cffad71d77d7b9d8c6b5d23da6e3
KimYoonMin <dbsalszz@naver.com>
2025-11-08 19:56:55 +0900

    주문 목록 조회 시 활용 DTO 분리: DTO 클래스, 레파지토리

---
+++ src/main/java/com/main/todo/dto/OrderSimpleQueryDto.java
@@ -0,0 +1,25 @@
+package com.main.todo.dto;
+
+import com.main.todo.domain.Address;
+import com.main.todo.domain.Order;
+import com.main.todo.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderSimpleQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
